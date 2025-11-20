3fc4410c29cddc1cd9ff205bd3c540d95ea52b99
McAyany <ayanyobala@gmail.com>
2025-11-20 23:25:00 +0300

    added application model

--- backend/src/models/Application.js
+++ backend/src/models/Application.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
+
 const appSchema = new mongoose.Schema({
   job: { type: mongoose.Schema.Types.ObjectId, ref: 'Job' },
   worker: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   status: { type: String, enum: ['pending','accepted','rejected'], default: 'pending' },
   coverNote: String
 }, { timestamps: true });
+
 export default mongoose.model('Application', appSchema);
